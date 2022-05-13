ba081db5c79c102831678ec14c656d719ccfc7a6
Taeri <taesaa@ttu.ee>
2022-05-13 12:28:22 +0300

    LineFollower assignment

---
+++ LineFollower/linefollower.py
@@ -0,0 +1,45 @@
+from FollowerBot import FollowerBot
+
+
+def katsetus(robot: FollowerBot):
+    """
+    Pane robot pisut liikuma. Vahet pole kui palju ja kuidas, testi natuke. :)
+
+    :param robot:
+    :return:
+    """
+
+
+def jooneni(robot: FollowerBot):
+    """
+    Robot sõidab sirge jooneni ja jääb seisma.
+    """
+
+
+def joonejalgija(robot: FollowerBot):
+    """
+    Robot sõidab joone (sirge, kumer, kõver) alguspunktini ja seejärel mööda joont selle lõpuni.
+    """
+
+
+if __name__ == "__main__":
+    """
+    Kustuta # selle rea eest, millist funktsiooni jooksutada soovid.
+    
+    Kõigi robotite algpositsioon on paika pandud start_x ja start_y muutujate all.
+    Tulemust näed samas kaustas robot_path.png failis.
+    """
+    # Esimene funktsioon on mõeldud katsetamiseks,
+    # katsetus(FollowerBot(start_x=250, start_y=300, track_image="testtrack.png"))
+    #
+    # Esimese ülesande käivitamiseks kustuta järgmiselt realt "#" ära
+    # jooneni(FollowerBot(start_x=250, start_y=300, track_image="straightline.png"))
+    #
+    # Teise ülesande kontrollimiseks kustuta järgmiselt realt "#" ära
+    # joonejalgija(FollowerBot(start_x=450, start_y=300, track_image="straightline.png"))
+    #
+    # Kolmanda ülesande kontrollimiseks kustuta järgmiselt realt "#" ära
+    # joonejalgija(FollowerBot(start_x=445, start_y=250, track_image="curve.png"))
+    #
+    # Neljanda ülesande kontrollimiseks kustuta järgmiselt realt "#" ära
+    # joonejalgija(FollowerBot(start_x=387, start_y=350, track_image="corner.png"))
