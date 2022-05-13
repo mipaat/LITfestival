fb9b75178afbddc7e452aa11aa6f5456d3b841c3
Lillkapsas <92450432+Lillkapsas@users.noreply.github.com>
2022-05-13 14:10:42 +0300

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,15 @@
+import random
+
+
+def guess(x):
+    """
+    Siia funktsiooni tuleks kirjutada mängu sisu. Funktsioon midagi tagastama ei pea, selle asemel kasuta
+    print().
+   :param x: 1-st kuni x-ni vahemik
+   """
+
+
+if __name__ == '__main__':
+    # Funktsiooni sisend määrab ära vahemiku lõpu, mis numbrini on võimalik arve ära arvata.
+    # Funktsiooni sisendit võite muuta.
+    guess(10)
